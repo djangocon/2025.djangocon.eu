e99186d328ec039086e7bd8625a33acddbcd2a71
Julius Boakye <66005635+Darkbeast-glitch@users.noreply.github.com>
2025-03-01 11:50:04 +0000

    Update/ticketbutton (#67)
    * feat: Update talk schedule with new links and styling enhancements
    
    * fix: Update scheduled talk links and improve styling in talks schedule
    
    * fix: Correct typo in scheduled talk header
    
    * add: Update speaker images and names on the home page
    
    * fix: Update speaker links and improve visa guide instructions
    
    * fix: Update speaker cards with correct links and names
    
    * fix: Add new sponsors to the default view
    
    * fix: Add Sentry as a new sponsor and update grant status styling
    
    * fix: Update ticket link in homepage to direct users to the ticketing site

--- djangocon_2025/site/views.py
+++ djangocon_2025/site/views.py
@@ -50,6 +50,12 @@ def default_view(request, menu="home", submenu=None):
                 "logo": "images/sponsors/it-schulungen.png",
                 "filter": False,
             },
+            {
+                "name": "Sentry",
+                "url": "https://sentry.io/",
+                "logo": "images/sponsors/sentry.svg",
+                "filter": True,
+            },
         ],
         "Sponsor": [],
         "Grants": [
