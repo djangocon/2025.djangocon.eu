363a66941be444a0732cdb9f687faa6a0b12a922
Julius Boakye <66005635+Darkbeast-glitch@users.noreply.github.com>
2025-04-03 08:29:31 +0000

    Add/add new sponsors (#74)
    * feat: Update talk schedule with new links and styling enhancements
    
    * fix: Update scheduled talk links and improve styling in talks schedule
    
    * fix: Correct typo in scheduled talk header
    
    * add: Update speaker images and names on the home page
    
    * fix: Update speaker links and improve visa guide instructions
    
    * fix: Update speaker cards with correct links and names
    
    * fix: Add new sponsors to the default view
    
    * fix: Add Sentry as a new sponsor and update grant status styling
    
    * fix: Update ticket link in homepage to direct users to the ticketing site
    
    * fix: Update childcare form deadline to March 31
    
    * feat: Add new sponsors Hamilton Rock and EuroPython Society
    
    * fix: Update sponsor logos to white versions and correct childcare form link
    
    * fix: Update childcare form link to the correct URL
    
    * feat: Add Divio as a new sponsor with logo and URL
    
    * refactor: Remove Hamilton Rock sponsor entry from views.py
    
    * feat: Update sponsor logos and add EuroPython Society and University of Porto
    
    * feat: Add 89grad as a new sponsor with logo and URL
    
    * feat: Add Maykin and Lincoln Loop as new sponsors with logos and URLs
    
    ---------
    
    Co-authored-by: Julius Boakye <bbjulius900@gmail.com>

--- djangocon_2025/site/views.py
+++ djangocon_2025/site/views.py
@@ -42,6 +42,14 @@ def default_view(request, menu="home", submenu=None):
                 "logo": "images/sponsors/89grad.png",
                 "filter": False,
             },
+
+            {
+                "name": "Maykin ",
+                "url": "https://www.maykinmedia.nl/nl/",
+                "logo": "images/sponsors/maykin_logo.png",
+                "filter": False,
+            },
+
         ],
         "Bronze": [
             {
@@ -74,6 +82,14 @@ def default_view(request, menu="home", submenu=None):
                 "logo": "images/sponsors/divio.png",
                 "filter": True,
             },
+
+            {
+                "name": "Lincoln Loop",
+                "url": "https://lincolnloop.com/",
+                "logo": "images/sponsors/lincoln.png",
+                "filter": True,
+            },
+
         ],
         "Sponsor": [],
         "Grants": [
